e9ee68c29f39ebdbbcc15ed70a979068c29461a4
Ben Malamud-Roam <bmalamudroam@gmail.com>
2020-12-09 16:18:24 -0800

    action dispatchers working

--- src/actions/currentVideo.js
+++ src/actions/currentVideo.js
@@ -1,5 +1,7 @@
 var changeVideo = (video) => ({
   //TODO:  Return some action object to change the currently playing video.
+  video: video,
+  type: 'CHANGE_VIDEO'
 });
 
 export default changeVideo;
--- src/actions/search.js
+++ src/actions/search.js
@@ -5,8 +5,19 @@ import YOUTUBE_API_KEY from '../config/youtube.js';
 
 
 var handleVideoSearch = (q) => {
- 
+
   //TODO:  Write an asynchronous action to handle a video search!
+  return (dispatch) => {
+    let options = {
+      key: YOUTUBE_API_KEY,
+      query: q,
+      max: 5
+    };
+    searchYouTube(options, (videoList) =>
+      dispatch(changeVideoList(videoList))
+    );
+  };
+
 };
 
 export default handleVideoSearch;
